3fa177a0ebc1ee1a27ed589547b6592e3bc90755
jiminh00 <jiminh1012@gmail.com>
2025-05-31 15:11:22 +0900

    feat(#7): FeignClient url 추가
    - reward, report 서비스 url 추가

--- src/main/java/com/modive/dashboard/client/RewardClient.java
+++ src/main/java/com/modive/dashboard/client/RewardClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "reward-service")
+@FeignClient(name="reward-service",
+        url="${service.reard.url}")
 public interface RewardClient {
 
     @PostMapping("/reward/earn")
