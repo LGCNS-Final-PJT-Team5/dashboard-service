dc42da55d82d4d1e6abfa2f0b8a63dbc867b4038
minjooong <kmj13675@gmail.com>
2025-06-10 10:40:54 +0900

    feat: AI Agent 호출 부분 환경변수로 변경

--- src/main/java/com/modive/dashboard/client/ReportClient.java
+++ src/main/java/com/modive/dashboard/client/ReportClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "report-service", url = "http://modive.site:60010")
+@FeignClient(name = "report-service",  url="${service.report.url}")
 public interface ReportClient {
 
     @PostMapping("/agent/weekly")
